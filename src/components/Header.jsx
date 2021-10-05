4b268cf035ca6c6835c0859c6aa239dabfd5663f
Billions94 <E.a.egbedi@gmail.com>
2021-10-05 23:00:53 +0200

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+const Header = () => (
+  <div className="container-fluid overflow-hidden p-0 pt-3">
+    {/*--genres*/}
+    <div className="row header sticky-top">
+      <h2 className="tv-shows pr-5">TV Shows</h2>
+      <div className="genreDiv mt-2">
+        <div
+          className="btn dropdown-toggle genrebtn text-light"
+          href="#"
+          role="button"
+          id="dropdownGenre"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          <span className="d-inline-block  genreSpan mr-3">Genres</span>
+        </div>
+        <div
+          className="dropdown-menu background"
+          aria-labelledby="dropdownGenre"
+        >
+          <a className="dropdown-item background" href="#">
+            Romance
+          </a>
+          <a className="dropdown-item background" href="#">
+            Comedy
+          </a>
+          <a className="dropdown-item background" href="#">
+            Fantasy
+          </a>
+        </div>
+      </div>
+      <div className="ml-auto d-flex  pb-1">
+        <div className="border">
+          <button className="btn borderbtn text-light">
+            <img src="./assets/rows.png" style={{ width: 14 }} />
+          </button>
+        </div>
+        <div className="border">
+          <button className="btn borderbtn text-light">
+            <img src="./assets/grid.png" style={{ width: 14 }} />
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Header;
